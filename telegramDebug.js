15498699b76f67250b9894e4ddcd26cd3494590e
AkshaayDev <163286127+AkshaayDev@users.noreply.github.com>
2024-05-16 09:45:14 +0000

    Update

--- telegramDebug.js
+++ telegramDebug.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const chatid = process.env.TELEGRAM_DEBUGCHAT;
 const token = process.env.TELEGRAM_TOKEN;
-const message = "Hello, World!";
+const message = encodeURIComponent((process.argv.length > 2)?process.argv.slice(2).join(" "):"Hello World");
 
 const send = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatid}&text=${message}`;
 const reset = `https://api.telegram.org/bot${token}/getUpdates?offset=-1`;
